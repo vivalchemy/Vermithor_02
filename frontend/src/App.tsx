25c1d077e31ab6399a562c6820d23c7e0ed95584
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2024-09-30 10:28:38 +0530

    theme-switcher

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { ModeToggle } from './components/mode-toggle'
+import { ThemeProvider } from './components/theme-provider'
 import { Button } from './components/ui/button'
 
 function App() {
 
   return (
-    <><div className='bg-white  text-black text-3xl'>
-      <Button>THis is a button</Button>
-    </div></>
+    <ThemeProvider>
+      <ModeToggle />
+      <Button>Hello</Button>
+    </ThemeProvider>
   )
 }
 
